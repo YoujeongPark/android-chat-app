b10ae9d2bdea1cf3ab1b56635fd4241a2af17cd4
YoujeongPark <pyj0695@gmail.com>
2020-12-03 11:12:22 +0900

    Firebase Database Erase After Start

--- app/src/main/java/com/example/chat_app/MainActivity.java
+++ app/src/main/java/com/example/chat_app/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // 추가
 
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference();
